b5d2620d7ea54ab041ecbeb89daa69f76d770ee9
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-04-08 12:44:50 +0530

    initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,270 @@
+import styles from "../css/Home.module.css";
+
+const OutcastersLandingPage = () => {
+  return (
+    <main className={styles.outcastersLandingPage}>
+      <img className={styles.bgImageIcon} alt="" src="/bgimage@2x.png" />
+      <img
+        className={styles.outcastersLandingPageChild}
+        alt=""
+        src="/ellipse-1.svg"
+      />
+      <img
+        className={styles.outcastersLandingPageItem}
+        alt=""
+        src="/ellipse-2.svg"
+      />
+      <img
+        className={styles.outcastersLandingPageInner}
+        alt=""
+        src="/ellipse-3.svg"
+      />
+      <img className={styles.ellipseIcon} alt="" src="/ellipse-4.svg" />
+      <img
+        className={styles.outcastersLandingPageChild1}
+        alt=""
+        src="/ellipse-6.svg"
+      />
+      <img
+        className={styles.outcastersLandingPageChild2}
+        alt=""
+        src="/ellipse-7.svg"
+      />
+      <img className={styles.icon} alt="" src="/1@2x.png" />
+      <img className={styles.icon1} alt="" src="/2@2x.png" />
+      <div className={styles.topNav}>
+        <div className={styles.frameParent}>
+          <div className={styles.frameChild} />
+          <b className={styles.outcasters}>Outcasters</b>
+        </div>
+        <div className={styles.forBrandsParent}>
+          <div className={styles.forBrands}>For Brands</div>
+          <div className={styles.forBrands}>About</div>
+          <div className={styles.forBrands}>Contact Us</div>
+          <div className={styles.loginWrapper}>
+            <div className={styles.forBrands}>Login</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.splittedText}>
+        <div className={styles.breakParent}>
+          <img className={styles.breakIcon} alt="" src="/0--break.svg" />
+          <img className={styles.freeIcon} alt="" src="/1--free.svg" />
+          <img className={styles.fromIcon} alt="" src="/2--from.svg" />
+          <img className={styles.normsIcon} alt="" src="/3--norms.svg" />
+        </div>
+        <div className={styles.breakParent}>
+          <img className={styles.thriveIcon} alt="" src="/4--thrive.svg" />
+          <img className={styles.uniquelyIcon} alt="" src="/5--uniquely.svg" />
+        </div>
+      </div>
+      <div className={styles.description}>
+        <div className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Id porta
+          nibh venenatis cras sed felis eget velit aliquet.
+        </div>
+        <div className={styles.startYourJourneyWrapper}>
+          <div className={styles.startYourJourney}>Start your Journey</div>
+        </div>
+      </div>
+      <div className={styles.div}>
+        <div className={styles.card1}>
+          <b className={styles.findMoreMeaningContainer}>
+            <p className={styles.findMore}>FIND MORE</p>
+            <p className={styles.findMore}>MEANING</p>
+          </b>
+          <div className={styles.loremIpsumDolor1}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas
+            accumsan lacus vel facilisis.
+          </div>
+        </div>
+        <div className={styles.card1}>
+          <b className={styles.findMoreMeaningContainer}>
+            <p className={styles.findMore}>THINK BIGGER</p>
+            <p className={styles.findMore}>ALL IS YOURS</p>
+          </b>
+          <div className={styles.loremIpsumDolor1}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas
+            accumsan lacus vel facilisis.
+          </div>
+        </div>
+        <div className={styles.card1}>
+          <b className={styles.findMoreMeaningContainer}>
+            <p className={styles.findMore}>THE FUTURE</p>
+            <p className={styles.findMore}>OF WORK</p>
+          </b>
+          <div className={styles.loremIpsumDolor1}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas
+            accumsan lacus vel facilisis.
+          </div>
+        </div>
+      </div>
+      <div className={styles.aboutParent}>
+        <img className={styles.aboutIcon} alt="" src="/about.svg" />
+        <img className={styles.outcastersIcon} alt="" src="/outcasters.svg" />
+        <img
+          className={styles.pngtree3dRotatingAbstractO}
+          alt=""
+          src="/pngtree3d-rotating-abstract-object-stereo-5428828-1@2x.png"
+        />
+        <div className={styles.loremIpsumDolor4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
+          euismod lacinia at quis risus sed. Enim nulla aliquet porttitor lacus
+          luctus accumsan tortor posuere. Id interdum velit laoreet id. Dolor
+          purus non enim praesent elementum facilisis leo. Ut ornare lectus sit
+          amet est placerat in egestas.
+        </div>
+        <div className={styles.readMoreWrapper}>
+          <div className={styles.forBrands}>Read more</div>
+        </div>
+      </div>
+      <div className={styles.theOutcastersParent}>
+        <b className={styles.theOutcasters}>THE OUTCASTERS</b>
+        <div className={styles.faceWithTearsOfJoyParent}>
+          <img
+            className={styles.faceWithTearsOfJoy}
+            alt=""
+            src="/face-with-tears-of-joy@2x.png"
+          />
+          <b className={styles.findMoreMeaningContainer}>OUR MISSION</b>
+          <div className={styles.loremIpsumDolor5}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas
+            accumsan lacus vel facilisis.
+          </div>
+        </div>
+        <div className={styles.winkingFaceParent}>
+          <img
+            className={styles.faceWithTearsOfJoy}
+            alt=""
+            src="/winking-face@2x.png"
+          />
+          <b className={styles.findMoreMeaningContainer}>OUR VISION</b>
+          <div className={styles.loremIpsumDolor5}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas
+            accumsan lacus vel facilisis.
+          </div>
+        </div>
+        <b className={styles.theOutcasters1}>THE OUTCASTERS</b>
+        <div className={styles.learnMoreParent}>
+          <div className={styles.forBrands}>Learn more</div>
+          <img className={styles.frameItem} alt="" src="/arrow-1.svg" />
+        </div>
+        <img className={styles.instagramIcon} alt="" src="/instagram.svg" />
+        <img className={styles.twitterIcon} alt="" src="/twitter.svg" />
+        <img className={styles.tiktokIcon} alt="" src="/tiktok.svg" />
+      </div>
+      <div className={styles.whatPeopleSayAboutUsParent}>
+        <b className={styles.whatPeopleSay}>WHAT PEOPLE SAY ABOUT US</b>
+        <div className={styles.frameGroup}>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameParent2}>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameInner} alt="" src="/frame-17@2x.png" />
+            <div className={styles.mrKontolkhapParent}>
+              <div className={styles.mrKontolkhap}>Mr. Kontolkhap</div>
+              <div className={styles.loremIpsumDolor7}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        className={styles.womanTakingFunnyPhotoWith}
+        alt=""
+        src="/woman-taking-funny-photo-with-dog@2x.png"
+      />
+      <div className={styles.joinOurCommunityParent}>
+        <div className={styles.joinOurCommunity}>Join our Community</div>
+        <div className={styles.loremIpsumDolor13}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+        <div className={styles.joinNowWrapper}>
+          <div className={styles.forBrands}>Join now</div>
+        </div>
+      </div>
+      <div className={styles.div1}>
+        <div className={styles.divwNodeDe1fc20d1aa61bea}>
+          <div className={styles.div2}>
+            <b className={styles.outcastersAllRight}>
+              © 2022 Outcasters. All right reserved.
+            </b>
+            <div className={styles.div3}>
+              <div className={styles.faq}>Faq</div>
+              <div className={styles.contact}>Contact</div>
+              <div className={styles.privacyPolicy}>privacy policy</div>
+              <div className={styles.termsOfUse}>Terms Of Use</div>
+            </div>
+          </div>
+          <div className={styles.svg} />
+        </div>
+        <img
+          className={styles.divwNodeDe1fc20d1aa61beaIcon}
+          alt=""
+          src="/divwnodede1fc20d1aa61beaf38cd40ece6fadebce6fadda.svg"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default OutcastersLandingPage;
